8b9af0a6d05673022c69034f7ea1a9e00bb0cc93
joni23008 <joni.mitronen@student.hamk.fi>
2024-01-27 12:05:52 +0200

    starting

---
+++ materiaali tehtava 0/index.php
@@ -0,0 +1,12 @@
+<?php
+include("header.html");
+?>
+
+<section>
+    <h2>Main Content Goes Here</h2>
+    <p>This is the content of my webpage.</p>
+</section>
+
+<?php
+include("footer.html");
+?>
---
+++ php/hello.php
@@ -0,0 +1,15 @@
+<?php
+  print "Hello PHP!kakaka";
+  $jalka=12;
+  $tuuma=2.54;
+  $mittayksikko="tuuma";
+  $totuus=True;
+  
+  print $jalka;
+  print "<br>"; //tulostukseen rivinvaihto 
+  print $tuuma;
+  print "<br>"; //tulostukseen rivinvaihto 
+  print $mittayksikko;
+  print "<br>"; //tulostukseen rivinvaihto 
+  print $totuus;
+?>
