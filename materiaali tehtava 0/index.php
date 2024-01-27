7f9e08a4fc70913d03c744abd250d4c5da393ef8
joni23008 <joni.mitronen@student.hamk.fi>
2024-01-27 12:20:28 +0200

    mtehtava 0

--- materiaali tehtava 0/index.php
+++ materiaali tehtava 0/index.php
@@ -2,10 +2,9 @@
 include("header.html");
 ?>
 
-<section>
-    <h2>Main Content Goes Here</h2>
-    <p>This is the content of my webpage.</p>
-</section>
+<?php
+echo "Terve. This is from index.php"
+?>
 
 <?php
 include("footer.html");
