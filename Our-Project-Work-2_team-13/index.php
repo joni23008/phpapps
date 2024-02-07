f1bb4a5dc97202d71f354cdf370448bd1972d305
joni23008 <joni.mitronen@student.hamk.fi>
2024-02-07 10:18:13 +0200

    edits joni rakenne html

---
+++ Our-Project-Work-2_team-13/index.php
@@ -0,0 +1,25 @@
+<?php
+//header
+include("header.html");
+//
+include("navbar.html");
+
+//mikä on sivun pääsisältö. 7 vaihtoehtoa. 
+//1. main
+include("main.html");
+//2. tietoa meistä
+//include("");
+//3. reseptit
+//include("");
+//4. viikon ruokalista
+//include("");
+//5. palaute-lomake
+//include("");
+//6. submit-ilmoitus
+//include("");
+//7. yhteysvirhe-ilmoitus
+//include("");
+
+//footer
+include("footer.html");
+?>
