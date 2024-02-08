bf2fab0cdf7a6d0f668061f0889ac56a35a51b21
HenriSoramaki <156062879+HenriSoramaki@users.noreply.github.com>
2024-02-08 11:13:50 +0200

    muutoksia

--- Our-Project-Work-2_team-13/pages-php/aboutus.php
+++ Our-Project-Work-2_team-13/pages-php/aboutus.php
@@ -7,24 +7,6 @@ include("../pages-html/includes/navbar_Joni.html");
 
 //About Us
 include("../pages-html/aboutus.html");
-<<<<<<< HEAD
-=======
-//mikä on sivun pääsisältö. 7 vaihtoehtoa. 
-//1. main
-include("../pages-html/main_Henkka.html");
-//2. tietoa meistä
-//include("");
-//3. reseptit
-include("../pages-html/reseptit_Joni.html");
-//4. viikon ruokalista
-include("../pages-html/ruokalista.html");
-//5. palaute-lomake
-include("../pages-html/palaute.html");
-//6. submit-ilmoitus
-//include("");
-//7. yhteysvirhe-ilmoitus
-//include("");
->>>>>>> ae932d5d2abccfaffd9ecd32bb84b6cc0b983099
 
 //footer
 include("../pages-html/includes/footer_Joni.html");
